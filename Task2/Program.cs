286000c3c67006790dfbc10b2ab1d143a4bc4698
Vladislav <Vlad.davidovich.2014@mail.ru>
2022-08-25 23:22:49 +0300

    Задача 2

---
+++ Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+Console.WriteLine("Введите числo ");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] array = new int [number];
+FillArray(array);
+PrintArray(array);
+int summ = 0;
+
+for (int i = 0; i < array.Length; i+=2)
+    summ = summ + array[i];
+
+void FillArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        {
+            array[i] = new Random().Next(1,100);
+        }
+}
+
+void PrintArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+}
+
+Console.WriteLine($"-> {summ}");
